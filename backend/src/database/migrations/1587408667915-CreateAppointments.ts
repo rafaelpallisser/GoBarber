06c3511296c240499cca6c7b7a66088d68c15da4
Rafael Pallisser <rafaelpallisser@gmail.com>
2020-04-20 21:34:41 -0300

    TypeORM repository

--- backend/src/database/migrations/1587408667915-CreateAppointments.ts
+++ backend/src/database/migrations/1587408667915-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587408667915
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
