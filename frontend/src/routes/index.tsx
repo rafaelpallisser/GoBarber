a15a0791a93b8e8198347030eae6f32b314ad6bb
Rafael Pallisser <rafaelpallisser@gmail.com>
2020-05-12 23:06:31 -0300

    Configurando Rotas

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, RouteChildrenProps } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
